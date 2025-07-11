35ab16917d29a3b46f1d9004a5cba0d08051d0a8
khushipansuriya1 <khushipansuriya24@gmail.com>
2025-07-11 10:12:48 +0530

    change

--- src/Pages/Listings.jsx
+++ src/Pages/Listings.jsx
@@ -68,7 +68,9 @@ const FilterBar = ({ filters, setFilters, clearFilters }) => {
       </div>
       <button
         onClick={clearFilters}
-        className="bg-stone-700 text-white px-4 py-2 rounded shadow w-auto hover:bg-stone-600 transition"
+        className="relative inline-block px-4 py-2 rounded font-medium text-white bg-stone-700 z-10 overflow-hidden w-auto
+    before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:bg-stone-600
+    before:z-[-1] before:transition-all before:duration-300 hover:before:w-full hover:text-white shadow transition"
       >
         Clear Filters
       </button>
