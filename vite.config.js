0686db1ef0d94d0df1ebc23a7c10cc542f3371d0
khushipansuriya1 <khushipansuriya24@gmail.com>
2026-01-29 21:36:54 +0530

    Fix blank page on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/real-estate/' : '/',
+export default defineConfig({
   plugins: [react(), tailwindcss(),],
-}))
+})
